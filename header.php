ad85b1df7012b3bcedd643070c52a7a9142c4e23
sonnek <rachelle@milesnorthproductions.com>
2018-08-07 13:51:37 -0500

    Move header php code to header-functions file.

--- header.php
+++ header.php
@@ -26,55 +26,21 @@
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$bulmatypic_description = get_bloginfo( 'description', 'display' );
-			if ( $bulmatypic_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $bulmatypic_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
+			
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="navbar" role="navigation" aria-label="main navigation">
 			<div class="navbar-brand">
-				<a class="navbar-item" href="https://bulma.io">
-					<img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
-				</a>
+				<?php bulmatypic_branding(); ?>
 
-				<button class="navbar-burger" aria-controls="primary-menu" aria-expanded="false">
+				<button class="navbar-burger" aria-expanded="false">
 					<span aria-hidden="true"></span>
 					<span aria-hidden="true"></span>
 					<span aria-hidden="true"></span>
 				</button>
 			</div>
 			<div id="primary-menu" class="navbar-menu menu">
-				<?php
-				wp_nav_menu( array(
-					'theme_location' => 'primary-menu',
-					'depth'          => 0,
-					'container'      => '',
-					'menu_class'     => 'navbar-start',
-					'fallback_cb'    => 'bulmapress_navwalker::fallback',
-					'walker'         => new Bulmatypic_Navwalker(),
-				));
-				wp_nav_menu( array(
-					'theme_location' => 'navbar-right',
-					'depth'          => 0,
-					'container'      => '',
-					'menu_class'     => 'navbar-end',
-					'fallback_cb'    => 'bulmapress_navwalker::fallback',
-					'walker'         => new Bulmatypic_Navwalker(),
-				));
-				?>
+				<?php bulmatypic_navbar(); ?>
 			</div>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
--- src/header-functions.php
+++ src/header-functions.php
@@ -6,25 +6,38 @@
  * @since 0.5.0
  */
 
-// Custom menu walker.
+// Custom navbar.
 function bulmatypic_navbar() {
-	$items_wrap = '<span id="%1$s" class="%2$s">%3$s</span>';
-		wp_nav_menu( array(
-			'theme_location' => 'primary-menu',
-			'depth'          => 0,
-			'container'      => 'div id="navigation"',
-			'menu_class'     => 'navbar-start',
-			'fallback_cb'    => 'bulmapress_navwalker::fallback',
-			'items_wrap'     => $items_wrap,
-			'walker'         => new Bulmatypic_Navwalker(),
-		));
-		wp_nav_menu( array(
-			'theme_location' => 'navbar-right',
-			'depth'          => 0,
-			'container'      => 'div id="navigation"',
-			'menu_class'     => 'navbar-end',
-			'items_wrap'     => $items_wrap,
-			'fallback_cb'    => 'bulmapress_navwalker::fallback',
-			'walker'         => new Bulmatypic_Navwalker(),
-		));
+	wp_nav_menu( array(
+		'theme_location' => 'primary-menu',
+		'depth'          => 0,
+		'container'      => '',
+		'menu_class'     => 'navbar-start',
+		'fallback_cb'    => 'Bulmapress_Navwalker::fallback',
+		'walker'         => new Bulmatypic_Navwalker(),
+	));
+	wp_nav_menu( array(
+		'theme_location' => 'navbar-right',
+		'depth'          => 0,
+		'container'      => '',
+		'menu_class'     => 'navbar-end',
+		'fallback_cb'    => 'Bulmapress_Nvwalker::fallback',
+		'walker'         => new Bulmatypic_Navwalker(),
+	));
+}
+
+function bulmatypic_branding() {
+	if ( has_custom_logo() ) :
+		the_custom_logo();
+	else :
+		if ( is_front_page() && is_home() ) :
+			echo '<h1 class="site-title"><a href="' . esc_url( home_url( '/' ) ) . 'rel="home">' .  bloginfo( 'name' ) . '</a></h1>';
+		else :
+			echo '<p class="site-title"><a href="' . esc_url( home_url( '/' ) ) . 'rel="home">' .  bloginfo( 'name' ) . '</a></p>';
+		endif;
+		$bulmatypic_description = get_bloginfo( 'description', 'display' );
+		if ( $bulmatypic_description || is_customize_preview() ) :
+			echo '<p class="site-description">' . $bulmatypic_description . '</p>'; /* WPCS: xss ok. */ 
+		endif;
+	endif; 
 }
